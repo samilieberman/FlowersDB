99085feb381c45d7b63ecd6df9d8407783a92a86
Daniel Tymecki <daniel.tymecki@gmail.com>
2018-12-11 18:54:10 -0500

    LORD AND SAVIOR
    The DATABASES IS DAVED

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms import ModelForm
+from .models import *
 
 
 class InsertForm(forms.Form):
@@ -7,8 +9,8 @@ class InsertForm(forms.Form):
     location = forms.CharField(max_length=30)
     sighted = forms.DateField()
 
-class UpdateForm(forms.Form):
-    name = forms.CharField(max_length=30)
-    person = forms.CharField(max_length=30)
-    location = forms.CharField(max_length=30)
-    sighted = forms.DateField()
+
+class UpdateForm(ModelForm):
+    class Meta:
+        model = Sightings
+        fields = ['name', 'person', 'location', 'sighted']
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -25,27 +25,16 @@ def index(request):
     else:
         form = InsertForm(prefix='insert')
 
-    if request.method == 'PUT' and not form.is_valid():
-        form2 = UpdateForm(request.PUT, prefix='update')
-        form = BannedPhraseForm(prefix='insert')
-        if form2.is_valid():
-            name = form2.cleaned_data['name']
-            person = form2.cleaned_data['person']
-            location = form2.cleaned_data['location']
-            sighted = form2.cleaned_data['sighted']
-            print('Updating new entry in sightings: ', name, person, location, sighted)
-            Sightings.objects.update(name=name, person=person, location=location, sighted=sighted)
-
-    else:
-        form2 = UpdateForm(prefix='update')
-
+    form2 = UpdateForm()
     latest_sightings = Sightings.objects.all()
     context = {'latest_sightings': latest_sightings, 'form': form, 'up_form': form2}
     return render(request, 'index.html', context)
 
+
 def insert(request):
     form = InsertForm()
     return render(request, 'insert.html', {'form': form})
+
 
 def signup(request):
     if request.method == 'POST':
@@ -61,6 +50,7 @@ def signup(request):
         r_form = UserCreationForm()
     return render(request, 'register.html', {'form3': r_form})
 
+
 def delete(request, id=None):
     print(id)
     inst = Sightings.objects.filter(id=id)
@@ -69,7 +59,16 @@ def delete(request, id=None):
 
 
 def update(request, id=None):
-    print(id)
+    print('Did this route right????')
     inst = Sightings.objects.filter(id=id)
-    inst.update()
-    return redirect("index")
+    form = UpdateForm(request.POST)
+    if form.is_valid():
+        #inst = form.save()
+        # inst.save()
+        print('Name after: ', Sightings._meta.get_field('name'))
+
+    form = InsertForm(prefix='insert')
+    form2 = UpdateForm()
+    latest_sightings = Sightings.objects.all()
+    context = {'latest_sightings': latest_sightings, 'form': form, 'up_form': form2}
+    return render(request, "index.html", context)
